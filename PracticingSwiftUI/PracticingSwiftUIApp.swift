24aa5d39e7a5d3ce83ca1cb6328c395c1469e467
knockknock <yangban11111@gmail.com>
2024-09-02 14:22:32 +0900

    Initial Commit

---
+++ PracticingSwiftUI/PracticingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticingSwiftUIApp.swift
+//  PracticingSwiftUI
+//
+//  Created by 양승혜 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
