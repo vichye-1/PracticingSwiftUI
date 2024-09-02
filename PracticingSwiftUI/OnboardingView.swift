67f6479d259dfca7e246a6e3a57f9509d10aee18
knockknock <yangban11111@gmail.com>
2024-09-03 01:16:51 +0900

    [Refactor] 가독성을 위해 파일명 변경

--- PracticingSwiftUI/OnboardingView.swift
+++ PracticingSwiftUI/OnboardingView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OnboardingView: View {
     var body: some View {
         NavigationView {
             VStack {
@@ -29,5 +29,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    OnboardingView()
 }
