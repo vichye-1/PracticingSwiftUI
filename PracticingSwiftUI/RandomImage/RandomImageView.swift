25142a57d7489cf12b7a4b14b6b905d3ee2e0ed0
knockknock <yangban11111@gmail.com>
2024-09-06 00:16:54 +0900

    [Feat] lorem picsum 사용하여 랜덤 이미지가 보여지도록 설정  - 한 섹션안의 이미지들이 모두 다르게 보여지게 하기위한 로직 설정  - 섹션마다 다른 이미지가 보여지도록 하는 로직 구현 : randomNumber 프로퍼티에 구현

--- PracticingSwiftUI/RandomImage/RandomImageView.swift
+++ PracticingSwiftUI/RandomImage/RandomImageView.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 
 struct RandomImageView: View {
+    
+    private let sectionTitle: [String] = ["First Section", "Second Section", "Third Section", "Forth Section"]
+    private let randomNumber: [Int] = (0..<4).map { _ in Int.random(in: 1...1000) }
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(spacing: 20) {
-                    titleStyle("First Section")
-                        .padding(.top, 20)
-                    titleStyle("Second Section")
-                    titleStyle("Third Section")
-                    titleStyle("Forth Section")
-                    
+                    ForEach(0..<4) { item in
+                        SectionStyle(sectionTitle[item], number: randomNumber[item])
+                            .padding(.top, 16)
+                    }
                     .navigationTitle("My Random Image")
                 }
             }
         }
     }
     
-    func titleStyle(_ title: String) -> some View {
+    func SectionStyle(_ title: String, number: Int) -> some View {
         VStack {
             Text(title)
                 .font(.title2)
@@ -32,16 +34,24 @@ struct RandomImageView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.horizontal, 16)
 
-            ScrollView(.horizontal) {
-                HStack(){
-                    Spacer(minLength: 10)
-                    ForEach(0..<10) { item in
-                        RoundedRectangle(cornerRadius: 25.0)
-                            .fill(.blue)
-                            .frame(width: 100, height: 150)
-                    }
-                    
+            randomImageView(number)
+        }
+    }
+}
+
+func randomImageView(_ randomNumber: Int) -> some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+        HStack(){
+            Spacer(minLength: 10)
+            ForEach(0..<10) { item in
+                AsyncImage(url: URL(string: "https://picsum.photos/id/\(randomNumber + item)/200/300")) { image in
+                    image
+                        .resizable()
+                } placeholder: {
+                    ProgressView()
                 }
+                .frame(width: 100, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 25.0))
             }
         }
     }
